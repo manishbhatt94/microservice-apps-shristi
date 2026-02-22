5741b22079865fc91c8bce10e4e80cf5853e07ec
Manish Bhatt <manishbhatt594@gmail.com>
2026-02-22 20:37:53 +0530

    Add scaffolding for greetapp that'll register as a client to Eureka reg
    Note the eureka-client dependency in pom.xml

---
+++ spring-greet-service/src/main/java/com/greeterapp/SpringGreetServiceApplication.java
@@ -0,0 +1,13 @@
+package com.greeterapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGreetServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGreetServiceApplication.class, args);
+	}
+
+}
---
+++ spring-greet-service/src/test/java/com/greeterapp/SpringGreetServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greeterapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGreetServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
