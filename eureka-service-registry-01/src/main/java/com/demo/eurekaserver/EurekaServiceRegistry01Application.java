e8e968713b7e112faaed8d4ea3f2a680dd64730b
Manish Bhatt <manishbhatt594@gmail.com>
2026-02-22 17:51:55 +0530

    Add scaffolding for springboot project that acts as a Eureka Registry
    Note the starter dependency of Eureka Server in pom.xml

---
+++ eureka-service-registry-01/src/main/java/com/demo/eurekaserver/EurekaServiceRegistry01Application.java
@@ -0,0 +1,13 @@
+package com.demo.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServiceRegistry01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServiceRegistry01Application.class, args);
+	}
+
+}
