08d58db93657cc6d8da7205c1fffbefe78277a95
Manish Bhatt <manishbhatt594@gmail.com>
2026-02-22 18:16:50 +0530

    Start adding basic configuration for Eureka Server

--- eureka-service-registry-01/src/main/java/com/demo/eurekaserver/EurekaServiceRegistry01Application.java
+++ eureka-service-registry-01/src/main/java/com/demo/eurekaserver/EurekaServiceRegistry01Application.java
@@ -2,8 +2,10 @@ package com.demo.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaServiceRegistry01Application {
 
 	public static void main(String[] args) {
