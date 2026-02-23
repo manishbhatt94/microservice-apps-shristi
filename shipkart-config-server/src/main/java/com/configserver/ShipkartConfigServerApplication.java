69e08d5f12ea7e4bd5bcd214c6008f0f540d1efb
Manish Bhatt <manishbhatt594@gmail.com>
2026-02-23 17:23:42 +0530

    Add boilerplate for project that will act as our config-server

---
+++ shipkart-config-server/src/main/java/com/configserver/ShipkartConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipkartConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShipkartConfigServerApplication.class, args);
+	}
+
+}
