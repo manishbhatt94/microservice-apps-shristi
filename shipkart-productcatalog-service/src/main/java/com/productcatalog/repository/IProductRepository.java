e44a46dbe4487d8b961330d6a3ffc3e41d002da8
Manish Bhatt <manishbhatt594@gmail.com>
2026-02-23 14:41:53 +0530

    Copy files from spring-restapi-productapp to productcatalog app

---
+++ shipkart-productcatalog-service/src/main/java/com/productcatalog/repository/IProductRepository.java
@@ -0,0 +1,17 @@
+package com.productcatalog.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.productcatalog.model.entities.Product;
+
+@Repository
+public interface IProductRepository extends JpaRepository<Product, Integer> {
+
+	@Query(value = "select p from Product p join p.categories c where c.categoryName = ?1")
+	List<Product> findByCategory(String category);
+
+}
