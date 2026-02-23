8d0548b51a5b917d27b9b8ec7ecfec9362b7b87c
Manish Bhatt <manishbhatt594@gmail.com>
2026-02-23 13:10:45 +0530

    Add boilerplate for another Eureka Client app called for productcatalog

---
+++ shipkart-productcatalog-service/src/main/java/com/productcatalog/ShipkartProductcatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.productcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipkartProductcatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShipkartProductcatalogServiceApplication.class, args);
+	}
+
+}
