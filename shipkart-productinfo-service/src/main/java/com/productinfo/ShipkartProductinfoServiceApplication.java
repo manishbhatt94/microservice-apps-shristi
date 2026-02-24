0efb574376415993687c097743e66aaaa1459b65
Manish Bhatt <manishbhatt594@gmail.com>
2026-02-24 16:36:38 +0530

    Add boilerplate for new app "product-info"
    This app will just get catalog data via Inter-Service Communication
    using Rest Clients from the main product-catalog app

---
+++ shipkart-productinfo-service/src/main/java/com/productinfo/ShipkartProductinfoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.productinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipkartProductinfoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShipkartProductinfoServiceApplication.class, args);
+	}
+
+}
