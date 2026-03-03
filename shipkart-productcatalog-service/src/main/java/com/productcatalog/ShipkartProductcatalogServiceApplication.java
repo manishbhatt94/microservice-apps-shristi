b966b8f226eaef4207d62940aa651cced4ab9239
Manish Bhatt <manishbhatt594@gmail.com>
2026-03-03 16:13:22 +0530

    Add feign to call inventory for creating inventory with initial stock

--- shipkart-productcatalog-service/src/main/java/com/productcatalog/ShipkartProductcatalogServiceApplication.java
+++ shipkart-productcatalog-service/src/main/java/com/productcatalog/ShipkartProductcatalogServiceApplication.java
@@ -3,9 +3,11 @@ package com.productcatalog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class ShipkartProductcatalogServiceApplication {
 
 	public static void main(String[] args) {
