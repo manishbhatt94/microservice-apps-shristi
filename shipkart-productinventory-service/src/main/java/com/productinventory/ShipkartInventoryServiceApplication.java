32a5f17a8b2a83ec76687c56c1f3cadefe2735bb
Manish Bhatt <manishbhatt594@gmail.com>
2026-03-03 13:04:14 +0530

    Add scaffolding for new app - inventory-service

---
+++ shipkart-productinventory-service/src/main/java/com/productinventory/ShipkartInventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.productinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipkartInventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShipkartInventoryServiceApplication.class, args);
+	}
+
+}
