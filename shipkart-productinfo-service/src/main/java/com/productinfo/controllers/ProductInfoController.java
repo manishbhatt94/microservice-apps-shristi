10d6b631eaf599d274a1530688842fa2dcfdc60a
Manish Bhatt <manishbhatt594@gmail.com>
2026-03-05 13:42:57 +0530

    Circuit Break when Info-Service can't reach Catalog-Service
    Note the CircuitBreaker annotation on service class methods.

--- shipkart-productinfo-service/src/main/java/com/productinfo/controllers/ProductInfoController.java
+++ shipkart-productinfo-service/src/main/java/com/productinfo/controllers/ProductInfoController.java
@@ -21,7 +21,8 @@ import com.productinfo.service.IProductInfoService;
 public class ProductInfoController {
 
 	// @Qualifier("productInfoServiceRestClientImpl")
-	@Qualifier("productInfoServiceFeignClientImpl")
+	// @Qualifier("productInfoServiceFeignClientImpl")
+	@Qualifier("productInfoServiceRCResilienceImpl")
 	@Autowired
 	private IProductInfoService productInfoService;
 
